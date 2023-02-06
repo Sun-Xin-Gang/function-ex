2e758a80b384fc5f9f9966139f8cd7ac96be63e3
Sun-Xin-Gang <xingang.sun@hotmail.com>
2023-02-06 15:34:11 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # function-ex
 Function throws Exception with Java 8
+## 简介 ( Chinese )
+这是一套对 java.util.function.* 函数式接口的扩展库，它们都默认抛出了 Exception，用来更简洁地构建你的代码。例如：
+```
+    void persist(Object o) throws SQLException {
+        // into db
+    }
+```
+
--- src/test/java/pers/sunrise/util/test/Test.java
+++ src/test/java/pers/sunrise/util/test/Test.java
@@ -1,7 +1,15 @@
 package pers.sunrise.util.test;
 
+import pers.sunrise.util.function.RunnableEx;
+
+import java.io.IOException;
+
 public class Test {
     public static void main(String... args) throws Exception {
-        Runnable run;
+        RunnableEx.without(Test::f).run();
+    }
+
+    public static void f() throws Exception {
+        throw new IOException();
     }
 }
